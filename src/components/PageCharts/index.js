1c1f1f82ad9f085eddf1435e71e5bf8c13208a94
makkahwi <suhaibahmadai@hotmail.com>
2022-06-30 19:16:09 +0300

    Collapse Charts & Statistics

--- src/components/PageCharts/index.js
+++ src/components/PageCharts/index.js
@@ -1,4 +1,4 @@
-import { Card, CardBody, CardHeader } from '../Root/Cards';
+import CollapseCard from '../CollapseCard';
 import { BarChart, DoughnutChart, LineChart, PieChart, PolarChart, RadarChart } from '../Root/Charts';
 import { Col, Row } from '../Root/Grid';
 import chartsDemoData from './demoData';
@@ -26,112 +26,106 @@ const rgbColors = color => {
   }
 };
 
-const PageCharts = ({ title, charts = chartsDemoData }) => {
+const PageCharts = ({ title = "Charts", charts = chartsDemoData }) => {
   return (
-    <Card className="mb-4">
-      <CardHeader>
-        {title || "Charts"}
-      </CardHeader>
+    <CollapseCard title={title}>
+      <Row>
+        {charts?.map((chart, i) => (
+          <Col sm={12} lg={6} className="px-5 my-3" key={i}>
+            <h5>
+              {chart.title}
+            </h5>
 
-      <CardBody>
-        <Row>
-          {charts?.map((chart, i) => (
-            <Col sm={12} lg={6} className="px-5 my-3" key={i}>
-              <h5>
-                {chart.title}
-              </h5>
-
-              {chart.type === "bar" ? (
-                <BarChart
-                  data={{
-                    labels: Object.keys(chart.data[0].data),
-                    datasets: chart.data.map(dat =>
-                    ({
-                      label: dat.title,
-                      backgroundColor: `rgba(${rgbColors(dat.color)},1)`,
-                      data: Object.values(dat.data)
-                    })
-                    )
-                  }}
-                />
-              ) : chart.type === "line" ? (
-                <LineChart
-                  data={{
-                    labels: Object.keys(chart.data[0].data),
-                    datasets: chart.data.map(dat =>
-                    ({
-                      label: dat.title,
-                      backgroundColor: `rgba(${rgbColors(dat.color)},0.2)`,
-                      borderColor: `rgba(${rgbColors(dat.color)},1)`,
-                      pointBackgroundColor: `rgba(${rgbColors(dat.color)},1)`,
-                      pointBorderColor: "#fff",
-                      data: Object.values(dat.data)
-                    })
-                    )
-                  }}
-                />
-              ) : chart.type === "doughnut" ? (
-                <DoughnutChart
-                  data={{
-                    labels: Object.keys(chart.data),
-                    datasets: [
-                      {
-                        backgroundColor: Object.values(chart.data).map((_, i) => `rgba(${rgbColors(colors[i % (colors.length - 1)])},1)`),
-                        data: Object.values(chart.data),
-                      },
-                    ],
-                  }}
-                />
-              ) : chart.type === "pie" ? (
-                <PieChart
-                  data={{
-                    labels: Object.keys(chart.data),
-                    datasets: [
-                      {
-                        data: Object.values(chart.data),
-                        backgroundColor: Object.values(chart.data).map((_, i) => `rgba(${rgbColors(colors[i % (colors.length - 1)])},1)`),
-                        hoverBackgroundColor: Object.values(chart.data).map((_, i) => `rgba(${rgbColors(colors[i % (colors.length - 1)])},1)`),
-                      },
-                    ],
-                  }}
-                />
-              ) : chart.type === "polar" ? (
-                <PolarChart
-                  data={{
-                    labels: Object.keys(chart.data),
-                    datasets: [
-                      {
-                        data: Object.values(chart.data),
-                        backgroundColor: Object.values(chart.data).map((_, i) => `rgba(${rgbColors(colors[i % (colors.length - 1)])},0.5)`),
-                        borderColor: Object.values(chart.data).map((_, i) => `rgba(${rgbColors(colors[i % (colors.length - 1)])},1)`),
-                      },
-                    ],
-                  }}
-                />
-              ) : chart.type === "radar" && (
-                <RadarChart
-                  data={{
-                    labels: Object.keys(chart.data[0].data),
-                    datasets: chart.data.map((dat, i) =>
-                    ({
-                      label: dat.title,
-                      backgroundColor: `rgba(${rgbColors(dat.color)},0.2)`,
-                      borderColor: `rgba(${rgbColors(dat.color)},1)`,
-                      pointBackgroundColor: `rgba(${rgbColors(dat.color)},1)`,
-                      pointHighlightStroke: `rgba(${rgbColors(dat.color)},1)`,
-                      pointBorderColor: '#fff',
-                      pointHighlightFill: '#fff',
-                      data: Object.values(dat.data)
-                    })
-                    )
-                  }}
-                />
-              )}
-            </Col>
-          ))}
-        </Row>
-      </CardBody>
-    </Card>
+            {chart.type === "bar" ? (
+              <BarChart
+                data={{
+                  labels: Object.keys(chart.data[0].data),
+                  datasets: chart.data.map(dat =>
+                  ({
+                    label: dat.title,
+                    backgroundColor: `rgba(${rgbColors(dat.color)},1)`,
+                    data: Object.values(dat.data)
+                  })
+                  )
+                }}
+              />
+            ) : chart.type === "line" ? (
+              <LineChart
+                data={{
+                  labels: Object.keys(chart.data[0].data),
+                  datasets: chart.data.map(dat =>
+                  ({
+                    label: dat.title,
+                    backgroundColor: `rgba(${rgbColors(dat.color)},0.2)`,
+                    borderColor: `rgba(${rgbColors(dat.color)},1)`,
+                    pointBackgroundColor: `rgba(${rgbColors(dat.color)},1)`,
+                    pointBorderColor: "#fff",
+                    data: Object.values(dat.data)
+                  })
+                  )
+                }}
+              />
+            ) : chart.type === "doughnut" ? (
+              <DoughnutChart
+                data={{
+                  labels: Object.keys(chart.data),
+                  datasets: [
+                    {
+                      backgroundColor: Object.values(chart.data).map((_, i) => `rgba(${rgbColors(colors[i % (colors.length - 1)])},1)`),
+                      data: Object.values(chart.data),
+                    },
+                  ],
+                }}
+              />
+            ) : chart.type === "pie" ? (
+              <PieChart
+                data={{
+                  labels: Object.keys(chart.data),
+                  datasets: [
+                    {
+                      data: Object.values(chart.data),
+                      backgroundColor: Object.values(chart.data).map((_, i) => `rgba(${rgbColors(colors[i % (colors.length - 1)])},1)`),
+                      hoverBackgroundColor: Object.values(chart.data).map((_, i) => `rgba(${rgbColors(colors[i % (colors.length - 1)])},1)`),
+                    },
+                  ],
+                }}
+              />
+            ) : chart.type === "polar" ? (
+              <PolarChart
+                data={{
+                  labels: Object.keys(chart.data),
+                  datasets: [
+                    {
+                      data: Object.values(chart.data),
+                      backgroundColor: Object.values(chart.data).map((_, i) => `rgba(${rgbColors(colors[i % (colors.length - 1)])},0.5)`),
+                      borderColor: Object.values(chart.data).map((_, i) => `rgba(${rgbColors(colors[i % (colors.length - 1)])},1)`),
+                    },
+                  ],
+                }}
+              />
+            ) : chart.type === "radar" && (
+              <RadarChart
+                data={{
+                  labels: Object.keys(chart.data[0].data),
+                  datasets: chart.data.map((dat, i) =>
+                  ({
+                    label: dat.title,
+                    backgroundColor: `rgba(${rgbColors(dat.color)},0.2)`,
+                    borderColor: `rgba(${rgbColors(dat.color)},1)`,
+                    pointBackgroundColor: `rgba(${rgbColors(dat.color)},1)`,
+                    pointHighlightStroke: `rgba(${rgbColors(dat.color)},1)`,
+                    pointBorderColor: '#fff',
+                    pointHighlightFill: '#fff',
+                    data: Object.values(dat.data)
+                  })
+                  )
+                }}
+              />
+            )}
+          </Col>
+        ))}
+      </Row>
+    </CollapseCard>
   )
 }
 
