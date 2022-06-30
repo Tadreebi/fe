c663cf7eb7ac4b0354e802d1b976010a6b71b14b
makkahwi <SuhaibAhmadAi@hotmail.com>
2022-06-30 14:59:02 +0300

    Input Central Components

---
+++ src/components/Root/InputFields/Input.js
@@ -0,0 +1,21 @@
+import { CFormInput } from '@coreui/react';
+
+const Input = ({ name, type, placeholder, title, required, value, onChange, disabled, children, ...rest }) => {
+  return (
+    <CFormInput
+      name={name}
+      type={type}
+      placeholder={placeholder || title}
+      required={required}
+      value={value}
+      onChange={onChange}
+      disabled={disabled}
+      {...rest}
+    >
+      {children}
+    </CFormInput>
+  )
+};
+
+export default Input;
+
