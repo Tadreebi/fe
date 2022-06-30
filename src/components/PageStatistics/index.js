4fa30b46c3da6f661d264a06ac4763a6efad85ee
makkahwi <SuhaibAhmadAi@hotmail.com>
2022-06-30 10:16:14 +0300

    Page Statistics Build

---
+++ src/components/PageStatistics/index.js
@@ -0,0 +1,217 @@
+import { CCard, CCardBody, CCardHeader, CCol, CRow, CWidgetStatsA } from '@coreui/react';
+import { CChartBar, CChartLine } from '@coreui/react-chartjs';
+import { getStyle } from '@coreui/utils';
+
+const statisticsDemo = [
+  {
+    color: "primary",
+    number: "26",
+    title: "Users",
+    chart: {
+      type: "line",
+      data: {
+        "Label 1": 70,
+        "Label 2": 60,
+        "Label 3": 40,
+        "Label 4": 50
+      },
+      fill: true
+    }
+  },
+  {
+    number: "26",
+    title: "Users",
+    chart: {
+      type: "line",
+      data: {
+        "Label 1": 70,
+        "Label 2": 60,
+        "Label 3": 40,
+        "Label 4": 50
+      },
+    }
+  },
+  {
+    number: "26",
+    title: "Users",
+    chart: {
+      type: "bar",
+      data: {
+        "Label 1": 7,
+        "Label 2": 6,
+        "Label 3": 4,
+        "Label 4": 5
+      },
+    }
+  }
+];
+
+const colors = [
+  "primary",
+  "info",
+  "success",
+  "warning",
+  "danger",
+  "dark",
+  "secondary"
+];
+
+const length = count => {
+  switch (count) {
+    case 1: return 12;
+    case 2: return 6;
+    case 3: return 4;
+    default: return 3;
+  }
+};
+
+const PageStatistics = ({ title, statistics = statisticsDemo }) => {
+  return (
+    <CCard className="mb-4">
+      <CCardHeader>
+        {title || "Statistics"}
+      </CCardHeader>
+
+      <CCardBody>
+        <CRow>
+          {statistics?.map((statistic, i) => (
+            <CCol sm={6} lg={length(statistics.length)} key={i}>
+              <CWidgetStatsA
+                className="mb-4"
+                color={statistic.color || colors[i % (colors.length - 1)]}
+                value={statistic.number}
+                title={statistic.title}
+                // action={
+                //   <CDropdown alignment="end">
+                //     <CDropdownToggle color="transparent" caret={false} className="p-0">
+                //       <CIcon icon={cilOptions} className="text-high-emphasis-inverse" />
+                //     </CDropdownToggle>
+                //     <CDropdownMenu>
+                //       <CDropdownItem>Action</CDropdownItem>
+                //     </CDropdownMenu>
+                //   </CDropdown>
+                // }
+                chart={
+                  statistic.chart && (
+                    <>
+                      {statistic.chart.type === "line" ? (
+                        <CChartLine
+                          className={statistic.chart.fill ? "mt-3" : "mt-3 mx-3"}
+                          data={{
+                            labels: Object.keys(statistic.chart.data),
+                            datasets: [
+                              {
+                                // label: 'My First dataset',
+                                backgroundColor: statistic.chart.fill ? 'rgba(255,255,255,.2)' : 'transparent',
+                                borderColor: 'rgba(255,255,255,.55)',
+                                pointBackgroundColor: getStyle(`--cui-${statistic.color || colors[i % (colors.length - 1)]}`),
+                                data: Object.values(statistic.chart.data),
+                                fill: statistic.chart.fill,
+                              },
+                            ],
+                          }}
+                          options={{
+                            plugins: {
+                              legend: {
+                                display: false,
+                              },
+                            },
+                            maintainAspectRatio: false,
+                            scales: {
+                              x: statistic.chart.fill ? {
+                                display: false,
+                              } : {
+                                grid: {
+                                  display: false,
+                                  drawBorder: false,
+                                },
+                                ticks: {
+                                  display: false,
+                                },
+                              },
+                              y: statistic.chart.fill ? {
+                                display: false,
+                              } : {
+                                min: 30,
+                                max: 89,
+                                display: false,
+                                grid: {
+                                  display: false,
+                                },
+                                ticks: {
+                                  display: false,
+                                },
+                              },
+                            },
+                            elements: {
+                              line: {
+                                borderWidth: 2,
+                                tension: 0.4,
+                              },
+                              point: {
+                                radius: statistic.chart.fill ? 0 : 4,
+                                hitRadius: 10,
+                                hoverRadius: 4,
+                              },
+                            },
+                          }}
+                        />
+                      ) : statistic.chart.type === "bar" && (
+                        <CChartBar
+                          className="mt-3 mx-3"
+                          data={{
+                            labels: Object.keys(statistic.chart.data),
+                            datasets: [
+                              {
+                                // label: 'My First dataset',
+                                backgroundColor: 'rgba(255,255,255,.2)',
+                                borderColor: 'rgba(255,255,255,.55)',
+                                data: Object.values(statistic.chart.data),
+                                // barPercentage: 0.6,
+                              },
+                            ],
+                          }}
+                          options={{
+                            maintainAspectRatio: false,
+                            plugins: {
+                              legend: {
+                                display: false,
+                              },
+                            },
+                            scales: {
+                              x: {
+                                grid: {
+                                  display: false,
+                                  drawTicks: false,
+                                },
+                                ticks: {
+                                  display: false,
+                                },
+                              },
+                              y: {
+                                grid: {
+                                  display: false,
+                                  drawBorder: false,
+                                  drawTicks: false,
+                                },
+                                ticks: {
+                                  display: false,
+                                },
+                              },
+                            },
+                          }}
+                        />
+                      )}
+                    </>
+                  )
+                }
+              />
+            </CCol>
+          ))}
+        </CRow>
+      </CCardBody>
+    </CCard>
+  )
+}
+
+export default PageStatistics
--- src/views/templatePage/index.js
+++ src/views/templatePage/index.js
@@ -1,12 +1,14 @@
 import PageForm from '../../components/PageForm'
 import PageHeader from '../../components/PageHeader'
 import PageTable from '../../components/PageTable'
+import PageStatistics from '../../components/PageStatistics'
 
-const TemplatePage = ({ pageTitle, pageDescrbition, formTitle, formInputs, onFormSubmit, onFormReset, tableTitle, tableData, onDataCreate, onDataEdit, onDataDelete, onActionSelection, currentAction }) => {
+const TemplatePage = ({ pageTitle, pageDescrbition, statisticsTitle, statisticsData, formTitle, formInputs, onFormSubmit, onFormReset, tableTitle, tableData, onDataCreate, onDataEdit, onDataDelete, onActionSelection, currentAction }) => {
 
   return (
     <>
       <PageHeader title={pageTitle} descrbition={pageDescrbition} />
+      {statisticsData && (<PageStatistics title={statisticsTitle} statistics={statisticsData} />)}
       <PageForm title={formTitle} inputs={formInputs} onSubmit={onFormSubmit} onReset={onFormReset} currentAction={currentAction} />
       <PageTable title={tableTitle} data={tableData} onCreate={onDataCreate} onEdit={onDataEdit} onDelete={onDataDelete} onActionSelection={onActionSelection} />
     </>
