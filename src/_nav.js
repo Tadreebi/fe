2f4fcf85e64350fddd601e83212b52160580af10
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-06-30 17:14:08 +0300

    Add form for companies to add tips for the students

--- src/_nav.js
+++ src/_nav.js
@@ -35,6 +35,12 @@ const _nav = [
     icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
   },
   {
+    component: CNavItem,
+    name: 'University Tips',
+    to: '/university-tips',
+    icon: <CIcon icon={cilSpeedometer} customClassName="nav-icon" />,
+  },
+  {
     component: CNavTitle,
     name: 'Template',
   },
