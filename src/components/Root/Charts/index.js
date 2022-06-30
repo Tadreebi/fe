112d5198abda147c5ea21d55c9bbf78b6dc5ad05
makkahwi <SuhaibAhmadAi@hotmail.com>
2022-06-30 15:28:07 +0300

    Chart Central Components

---
+++ src/components/Root/Charts/index.js
@@ -0,0 +1,16 @@
+
+import BarChart from '../BarChart';
+import DoughnutChart from '../DoughnutChart';
+import LineChart from '../LineChart';
+import PieChart from '../PieChart';
+import PolarChart from '../PolarChart';
+import RadarChart from '../RadarChart';
+
+export {
+  BarChart,
+  DoughnutChart,
+  LineChart,
+  PieChart,
+  PolarChart,
+  RadarChart
+}
