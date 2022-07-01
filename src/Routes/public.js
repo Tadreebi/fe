c49f0ba914feebcea29a919fcdc5aa61c5bd82fe
makkahwi <suhaibahmadai@hotmail.com>
2022-07-02 00:36:58 +0300

    Initia Sutndet Profile Page

--- src/Routes/public.js
+++ src/Routes/public.js
@@ -1,22 +1,24 @@
-import React from 'react'
+import { lazy } from 'react';
 
 // Modules
-const Login = React.lazy(() => import('../views/Public/Login'));
-const PasswordReset = React.lazy(() => import('../views/Public/PasswordReset'));
-const PasswordChange = React.lazy(() => import('../views/Public/PasswordChange'));
-const StudentRegister = React.lazy(() => import('../views/Public/StudentRegister'));
-const UniversityRegister = React.lazy(() => import('../views/Public/UniversityRegister'));
-const CompanyRegister = React.lazy(() => import('../views/Public/CompanyRegister'));
+const Login = lazy(() => import('../views/Public/Login'));
+const PasswordReset = lazy(() => import('../views/Public/PasswordReset'));
+const PasswordChange = lazy(() => import('../views/Public/PasswordChange'));
+const StudentRegister = lazy(() => import('../views/Public/StudentRegister'));
+const UniversityRegister = lazy(() => import('../views/Public/UniversityRegister'));
+const CompanyRegister = lazy(() => import('../views/Public/CompanyRegister'));
+const StudentProfile = lazy(() => import('src/views/Public/StudentProfile'));
 
 // Errors
-const Page404 = React.lazy(() => import('../views/Errors/404'));
-const Page500 = React.lazy(() => import('../views/Errors/500'));
+const Page404 = lazy(() => import('../views/Errors/404'));
+const Page500 = lazy(() => import('../views/Errors/500'));
 
 const routes = [
   { path: '/', exact: true, name: 'Home', element: Login },
 
   // Auth
   { path: '/login', name: 'Home', element: Login },
+  { path: '/profile', name: 'Profile', element: StudentProfile },
   { path: '/password/reset', name: 'Reset Password', element: PasswordReset },
   { path: '/password/change', name: 'Change Password', element: PasswordChange },
   { path: '/register/student', name: 'Student Register', element: StudentRegister },
