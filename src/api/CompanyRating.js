544df288fca78e981acae7af6f40794d6089abbb
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-07-04 21:25:57 +0300

    Company Rating API Call

---
+++ src/api/CompanyRating.js
@@ -0,0 +1,35 @@
+import api from ".";
+
+const path = "company-ratings/";
+
+//// Responses
+const getAllScores = params => {
+    return api.get(`${path}`, { params });
+  };
+
+  const getScore = id => {
+    return api.get(`${path}${id}`);
+  };
+
+  const createScore = data => {
+    return api.post(`${path}create/`, data);
+  };
+
+  const updateScore = (id, data) => {
+    return api.put(`${path}update/${id}`, data);
+  };
+
+  const deleteScore = id => {
+    return api.delete(`${path}delete/${id}`);
+  };
+
+
+
+
+  export default {
+    getAllScores,
+    getScore,
+    createScore,
+    updateScore,
+    deleteScore,
+  };
